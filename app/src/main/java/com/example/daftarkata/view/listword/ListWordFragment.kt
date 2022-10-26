80e95d5b23c0f8a0b818bc21539d959bce3e06a0
Reditya02 <putureditya@gmail.com>
2022-10-26 14:10:47 +0700

    Code Cleanup

--- app/src/main/java/com/example/daftarkata/view/listword/ListWordFragment.kt
+++ app/src/main/java/com/example/daftarkata/view/listword/ListWordFragment.kt
@@ -22,7 +22,7 @@ class ListWordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_letter, container, false)
+        return inflater.inflate(R.layout.fragment_list_word, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
